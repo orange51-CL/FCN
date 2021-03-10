7fd6f7c2eb86c8257b5c1218650b19691f7ceff2
orange51-CL <862409782@qq.com>
2021-03-10 15:11:52 +0800

    数据集读取

--- datasets.py
+++ datasets.py
@@ -16,29 +16,30 @@ voc_colormap = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128],
                 [64, 128, 0], [192, 128, 0], [64, 0, 128], [192, 0, 128], [64, 128, 128],
                 [192, 128, 128], [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
                 [0, 64, 128]]
-###
+
 
 class Datasets:
     def read_images(self):
-        transform = transforms.Compose([transforms.ToTensor()])
+        # 对图片进行初始化
+        # transforms.Normalize 对数据按通道进行标准化，即先减均值，再除以标准差
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+             # transforms.Resize((224, 224)),
+             # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+             ])
 
-        self.trainset = datasets.VOCSegmentation(root=voc_path, year='2012', image_set='train',
-                                                 download=True, transform=transform)
-        print(321)
-        self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=4,
-                                                       shuffle=True, num_workers=2)
+        self.trainset = datasets.VOCSegmentation(root='data/', year='2012', image_set='train',
+                                                 download=False, transform=transform)
+        self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=1, drop_last=True,
+                                                       collate_fn=lambda x: x, shuffle=True, num_workers=0)
 
         return self.trainset, self.trainloader
 
 
 if __name__ == '__main__':
     trainset, trainloader = Datasets().read_images()
-    dataiter = iter(trainloader)
 
-    print(123)
+    print(len(trainloader), type(trainloader), trainloader)
 
-    images, labels = dataiter.next()
-    # 展示图像
-    Other().imshow(torchvision.utils.make_grid(images))
-    # 显示图像标签
-    print(' '.join('%5s' % voc_classes[labels[j]] for j in range(4)))
+    for labels, data in enumerate(trainloader, 0):
+        print(labels)
